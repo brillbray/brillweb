f72557374a2e8e9a8b4c59ad4f8f46da92bb4872
Brillian Braydent <brillbrizz128@gmail.com>
2026-04-02 19:48:39 +0700

    Updates

--- src/assets/TANAKA GUITAR/javaFaq.js
+++
@@ -1,6 +0,0 @@
-function burgerMenu(){
-    var burgerMenu = document.querySelector('.burger');
-    var nav = document.querySelector('.nav');
-    burgerMenu.classList.toggle('active'); 
-    nav.classList.toggle('active'); 
-}
